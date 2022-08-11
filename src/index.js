3c6d68315609520b624d0f26bae25d2baea440ba
VabGen <vabgen@gmail.com>
2022-08-11 22:37:03 +0300

    fix: declaration of constants in a file index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = {
+    "}": "{",
+    "]": "[",
+    ")": "(",
+    "|": "|"
+  };
+
+  const st = str.split('');
+  const unite = [...st, ...bracketsConfig];
+  const removeSubarrays = unite.reduce((acc, val) => acc.concat(val), []);
+  const array = [];
+
+  for (let i = 0; i < removeSubarrays.length; i++) {
+    const closedBracket = (bracket) => ["}", "]", ")"].indexOf(bracket) > -1;
+    if (closedBracket(removeSubarrays[i])) {
+      if (removeSubarrays[i] !== brackets["|"] && brackets[removeSubarrays[i]] !== array.pop()) {
+        return false;
+      }
+    } else if (removeSubarrays[i] == brackets["|"] && brackets["|"] == array.pop()) {
+      array.pop();
+    } else {
+      array.push(removeSubarrays[i])
+    }
+  }
+  return array.length === 0;
 }
